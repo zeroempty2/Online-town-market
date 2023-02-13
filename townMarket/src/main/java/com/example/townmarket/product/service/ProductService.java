2e544c2d757fbc3d41b4181151d20969493c08a3
Seongjae <cdaosldk@naver.com>
2023-02-13 17:21:11 +0900

    #42 <refactor> Product Entity/ Service/ ServiceImpl/ Controller/ Repository/ ResponseDto/ PagingProductResponse/ ServiceImplTest 코드 리팩토링

--- townMarket/src/main/java/com/example/townmarket/product/service/ProductService.java
+++ townMarket/src/main/java/com/example/townmarket/product/service/ProductService.java
@@ -4,28 +4,23 @@ import com.example.townmarket.commons.dto.PageDto;
 import com.example.townmarket.product.dto.PagingProductResponse;
 import com.example.townmarket.product.dto.ProductRequestDto;
 import com.example.townmarket.product.dto.ProductResponseDto;
-import com.example.townmarket.product.entity.Product;
 import com.example.townmarket.user.entity.User;
-import java.util.List;
 import org.springframework.data.domain.Page;
 
 public interface ProductService {
 
   // 상품 생성
-  String addProduct(User user, ProductRequestDto productDto);
+  void addProduct(User user, ProductRequestDto productDto);
 
   // 단일 상품 조회
-  ProductResponseDto showProduct(long productId);
+  ProductResponseDto showProduct(Long productId);
 
   // 전체 상품 조회
   Page<PagingProductResponse> viewAllProduct(PageDto pageDto);
 
   // 상품 내용 수정
-  String updateProduct(long productId, ProductRequestDto productDto, User user);
+  void updateProduct(Long productId, ProductRequestDto productDto, User user);
 
   // 상품 삭제
-  void deleteProduct(long productId, User user);
-
-  List<Product> findAllProduct();
-
+  void deleteProduct(Long productId, User user);
 }
--- townMarket/src/main/java/com/example/townmarket/product/service/ProductServiceImpl.java
+++ townMarket/src/main/java/com/example/townmarket/product/service/ProductServiceImpl.java
@@ -7,11 +7,8 @@ import com.example.townmarket.product.dto.ProductResponseDto;
 import com.example.townmarket.product.entity.Product;
 import com.example.townmarket.product.repository.ProductRepository;
 import com.example.townmarket.user.entity.User;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,80 +20,63 @@ public class ProductServiceImpl implements ProductService {
 
   @Override
   @Transactional
-  public String addProduct(User user, ProductRequestDto productRequestDto) {
+  public void addProduct(User user, ProductRequestDto productRequestDto) {
     Product product = Product.builder()
         .productName(productRequestDto.getProductName())
         .productPrice(productRequestDto.getProductPrice())
         .productStatus(productRequestDto.getProductStatus())
         .productCategory(productRequestDto.getProductCategory())
         .productEnum(productRequestDto.getProductEnum())
-        .userId(user.getId())
+        .user(user)
         .build();
     productRepository.save(product);
-    return "상품을 성공적으로 등록했습니다";
   }
 
   @Override
-  @Transactional
-  public ProductResponseDto showProduct(long productId) {
+  @Transactional(readOnly = true)
+  public ProductResponseDto showProduct(Long productId) {
     Product product = findProductById(productId);
-
-    return new ProductResponseDto(
-        product.getId(),
-        product.getProductName(),
-        product.getProductPrice(),
-        product.getProductStatus(),
-        product.getProductCategory(),
-        product.getProductEnum()
-    );
+    return ProductResponseDto.builder()
+        .productId(product.getId())
+        .productName(product.getProductName())
+        .productPrice(product.getProductPrice())
+        .productStatus(product.getProductStatus())
+        .productCategory(product.getProductCategory())
+        .build();
   }
 
   @Override
   @Transactional
-  public Page<PagingProductResponse> viewAllProduct(PageDto pageDto) { //페이지로 받아오게
-    List<Product> products = findAllProduct();
-    return new PageImpl<>(
-        products.stream().map(PagingProductResponse::new).collect(Collectors.toList()),
-        pageDto.toPageable(),
-        pageDto.getSize());
+  public Page<PagingProductResponse> viewAllProduct(PageDto pageDto) {
+    Page<Product> products = productRepository.findAll(pageDto.toPageable());
+    return products.map(PagingProductResponse::new);
   }
 
   @Override
   @Transactional
-  public String updateProduct(long productId, ProductRequestDto productDto, User user) {
+  public void updateProduct(Long productId, ProductRequestDto productDto, User user) {
     Product product = findProductById(productId);
-    if (user.getId().equals(productId)) {
-      product.update(productDto);
-      return "상품이 성공적으로 업데이트되었습니다";
-    } else {
+    if (!user.checkAuthorization(user)) {
       throw new IllegalArgumentException("본인의 상품이 아닙니다");
     }
+    product.update(productDto);
   }
 
   @Override
   @Transactional
-  public void deleteProduct(long productId, User user) {
-    Product product = productRepository.findById(productId).orElseThrow(
-        () -> new IllegalArgumentException("존재하지 않는 상품입니다")
-    );
+  public void deleteProduct(Long productId, User user) {
+    Product product = findProductById(productId);
 
-    if (user.getId().equals(productId)) {
-      productRepository.deleteById(product.getId());
-    } else {
+    if (!user.checkAuthorization(user)) {
       throw new IllegalArgumentException("본인의 상품이 아닙니다");
     }
-  }
-
-  @Override
-  public List<Product> findAllProduct() {
-    return productRepository.findAll();
+    productRepository.deleteById(product.getId());
   }
 
   public Product findProductById(Long productId) {
-    Product product = productRepository.findById(productId).orElseThrow(
+    return productRepository.findById(productId).orElseThrow(
         () -> new IllegalArgumentException("존재하지 않는 상품입니다")
     );
-    return product;
   }
 
 }
