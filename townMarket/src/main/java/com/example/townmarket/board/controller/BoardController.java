6496aaba58d2b32499df3c5ad4698644344cd7e3
EUIJOON <euijoonism@gmail.com>
2023-02-14 09:59:12 +0900

    <refactor> Board - Board, BoardController, BoardResponse, BoardRequest, BoardService, BoardServiceImpl, PagingBoardResponse - 게시판 코드 리펙토링

--- townMarket/src/main/java/com/example/townmarket/board/controller/BoardController.java
+++ townMarket/src/main/java/com/example/townmarket/board/controller/BoardController.java
@@ -1,14 +1,18 @@
 package com.example.townmarket.board.controller;
 
 import com.example.townmarket.board.dto.BoardResponseDto;
-import com.example.townmarket.board.dto.CreateBoardRequestDto;
+import com.example.townmarket.board.dto.BoardRequestDto;
+import com.example.townmarket.board.dto.PagingBoardResponse;
 import com.example.townmarket.board.service.BoardService;
 import com.example.townmarket.commons.dto.PageDto;
 import com.example.townmarket.commons.dto.StatusResponseDto;
+import com.example.townmarket.commons.responseMessageData.DefaultResponse;
+import com.example.townmarket.commons.responseMessageData.ResponseMessages;
 import com.example.townmarket.commons.security.UserDetailsImpl;
 import com.example.townmarket.commons.util.SetHttpHeaders;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,46 +32,50 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
   private final BoardService boardService;
-
-  private final SetHttpHeaders setHttpHeaders;
+  private final SetHttpHeaders httpHeaders;
 
   // 게시글 생성
   @PostMapping
-  public ResponseEntity<StatusResponseDto> createBoard(
-      @RequestBody CreateBoardRequestDto createBoardRequestDto,
+  public ResponseEntity<DefaultResponse> createBoard(
+      @RequestBody BoardRequestDto boardRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return ResponseEntity.status(HttpStatus.CREATED)
-        .body(new StatusResponseDto(HttpStatus.CREATED.value(), boardService.CreateBoard(
-            createBoardRequestDto, userDetails.getUser())));
+    boardService.createBoard(boardRequestDto, userDetails.getUser());
+    DefaultResponse defaultResponse = DefaultResponse.valueOf(ResponseMessages.CREATED_SUCCESS);
+    return ResponseEntity.status(HttpStatus.CREATED).body(defaultResponse);
   }
 
   // 게시글 수정
   @PutMapping("/{boardId}")
-  public ResponseEntity<StatusResponseDto> updateBoard(@PathVariable long boardId,
-      @RequestBody CreateBoardRequestDto createBoardRequestDto,
+  public ResponseEntity<DefaultResponse> updateBoard(@PathVariable Long boardId,
+      @RequestBody BoardRequestDto boardRequestDto,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return ResponseEntity.status(HttpStatus.OK).body(new StatusResponseDto(HttpStatus.OK.value(),
-        boardService.updateBoard(boardId, createBoardRequestDto, userDetails.getUser())));
+    boardService.updateBoard(boardId, boardRequestDto, userDetails.getUser());
+    DefaultResponse defaultResponse = DefaultResponse.valueOf(ResponseMessages.SUCCESS);
+    return ResponseEntity.ok().body(defaultResponse);
+
   }
 
   // 게시글 단건 조회, 댓글 목록으로 불러오게 추가(페이징)
   @GetMapping("/{boardId}")
-  public ResponseEntity<BoardResponseDto> getBoard(@PathVariable long boardId) {
-    return ResponseEntity.status(HttpStatus.OK).headers(setHttpHeaders.setHeaderTypeJson())
+  public ResponseEntity<BoardResponseDto> getBoard(@PathVariable Long boardId) {
+    HttpHeaders headers = httpHeaders.setHeaderTypeJson();
+    return ResponseEntity.ok().headers(headers)
         .body(boardService.getBoard(boardId));
   }
 
   // 게시글 전체 조회
   @GetMapping
-  public ResponseEntity<Page<BoardResponseDto>> getBoards(@RequestBody PageDto pageDto) {
-    return ResponseEntity.status(HttpStatus.OK).body(boardService.getBoards(pageDto));
+  public ResponseEntity<Page<PagingBoardResponse>> getBoards(@RequestBody PageDto pageDto) {
+    HttpHeaders headers = httpHeaders.setHeaderTypeJson();
+    return ResponseEntity.ok().headers(headers).body(boardService.getBoards(pageDto));
   }
 
   // 게시물 삭제
   @DeleteMapping("{boardId}")
-  public ResponseEntity<StatusResponseDto> deleteBoard(@PathVariable long boardId,
+  public ResponseEntity<DefaultResponse> deleteBoard(@PathVariable Long boardId,
       @AuthenticationPrincipal UserDetailsImpl userDetails) {
-    return ResponseEntity.status(HttpStatus.OK).body(new StatusResponseDto(HttpStatus.OK.value(),
-        boardService.deleteBoard(boardId, userDetails.getUser())));//삭제시는 no contents
+    boardService.deleteBoard(boardId, userDetails.getUser());
+    DefaultResponse defaultResponse = DefaultResponse.valueOf(ResponseMessages.DELETE_SUCCESS);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(defaultResponse);
   }
 }
--- townMarket/src/main/java/com/example/townmarket/board/service/BoardServiceImpl.java
+++ townMarket/src/main/java/com/example/townmarket/board/service/BoardServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.townmarket.board.service;
 
 import com.example.townmarket.board.dto.BoardResponseDto;
-import com.example.townmarket.board.dto.CreateBoardRequestDto;
+import com.example.townmarket.board.dto.BoardRequestDto;
+import com.example.townmarket.board.dto.PagingBoardResponse;
 import com.example.townmarket.board.entity.Board;
 import com.example.townmarket.board.repository.BoardRepository;
 import com.example.townmarket.commons.dto.PageDto;
+import com.example.townmarket.product.repository.ProductRepository;
 import com.example.townmarket.user.entity.User;
 import jakarta.transaction.Transactional;
 import java.util.List;
@@ -17,67 +19,69 @@ import org.springframework.stereotype.Service;
 public class BoardServiceImpl implements BoardService {
 
   private final BoardRepository boardRepository;
+  private final ProductRepository productRepository;
 
   // 게시글 생성
   @Override
   @Transactional
-  public String CreateBoard(CreateBoardRequestDto createBoardRequestDto, User user) {
-    Board board = new Board(createBoardRequestDto.getTitle(), createBoardRequestDto.getContent(),
-        createBoardRequestDto.getSubject(), user);//빌더 사용 고려
+  public void createBoard(BoardRequestDto boardRequestDto, User user) {
+    Board board = Board.builder()
+        .title(boardRequestDto.getTitle())
+        .content(boardRequestDto.getContent())
+        .subject(boardRequestDto.getSubject())
+        .user(user)
+        .build();
     boardRepository.save(board);
-    return "게시글이 성공적으로 생성되었습니다";
   }
 
   // 게시글 수정
   @Override
   @Transactional
-  public String updateBoard(long boardId, CreateBoardRequestDto createBoardRequestDto,
-      User user) {  //CreateBoardRequestDto -> BoardRequestDto
-    Board board = boardRepository.findById(boardId)
-        .orElseThrow(() -> new IllegalArgumentException("게시글 id 없음"));  //메서드로 분리해서 사용
-
-    if (board.checkBoardWriter(user)) {
-      board.update(createBoardRequestDto);
-      return "게시글이 수정되었습니다";
-    } else {
-      throw new IllegalArgumentException("수정이 완료되지 않았습니다");
+  public void updateBoard(Long boardId, BoardRequestDto boardRequestDto,
+      User user) {
+    Board board = findBoardById(boardId);
+    if (!board.checkBoardWriter(user)) {
+      throw new IllegalArgumentException("본인 게시글이 아닙니다");
     }
+    board.update(boardRequestDto);
   }
 
   // 게시글 전체 조회
   @Override
   @Transactional
-  public Page<BoardResponseDto> getBoards(PageDto pageDto) {
+  public Page<PagingBoardResponse> getBoards(PageDto pageDto) {
     Page<Board> boards = boardRepository.findAll(pageDto.toPageable());
-    return boards.map(BoardResponseDto::new);
+    return boards.map(PagingBoardResponse::new);
   }
+
+
 
   // 게시글 단건 조회
   @Override
   @Transactional
-  public BoardResponseDto getBoard(long boardId) {
-    Board board = boardRepository.findById(boardId)
-        .orElseThrow(() -> new IllegalArgumentException("게시글 id 없음"));
-
-    return new BoardResponseDto(board);
+  public BoardResponseDto getBoard(Long boardId) {
+    Board board = findBoardById(boardId);
+    return BoardResponseDto.builder()
+        .title(board.getTitle())
+        .content(board.getContent())
+        .subject(board.getSubject())
+        .build();
   }
 
   // 게시글 삭제
   @Override
   @Transactional
-  public String deleteBoard(long boardId, User user) {
-    Board board = boardRepository.findById(boardId)
-        .orElseThrow(() -> new IllegalArgumentException("게시글 id 없음"));
-
-    if (board.checkBoardWriter(user)) {
-      boardRepository.delete(board);
-      return "삭제가 완료되었습니다";
-    } else {
-      throw new IllegalArgumentException("삭제가 불가능합니다");
+  public void deleteBoard(Long boardId, User user) {
+    Board board = findBoardById(boardId);
+    if(!board.checkBoardWriter(user)) {
+      throw new IllegalArgumentException("본인의 게시글이 아닙니다.");
     }
+    productRepository.deleteById(board.getId());
+
   }
 
-  public List<Board> findAllBoard() {
-    return boardRepository.findAll();
+  // 중복 로직 메서드 분리
+  public Board findBoardById(Long boardId) {
+    return boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("게시글이 없습니다."));
   }
 }
