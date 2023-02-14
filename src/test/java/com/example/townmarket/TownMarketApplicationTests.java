6e1521c3843823bf0e8ce67ee5bbf61c3fc749df
skyriv213 <gn1007@dankook.ac.kr>
2023-02-14 16:45:54 +0900

    #61 <refactor> (<town>) : CI- git action를 위한 경로 수정

---
+++ src/test/java/com/example/townmarket/TownMarketApplicationTests.java
@@ -0,0 +1,14 @@
+//package com.example.townmarket;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//@SpringBootTest
+//class TownMarketApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
