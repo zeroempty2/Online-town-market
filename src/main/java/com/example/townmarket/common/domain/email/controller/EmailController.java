4576491447cdabc148bf5396d2965076a3aa0b73
Seowoo <5w31892p@gmail.com>
2023-02-16 09:40:43 +0900

    #71 <fix> emailcontroller - getmapping delete

--- src/main/java/com/example/townmarket/common/domain/email/controller/EmailController.java
+++ src/main/java/com/example/townmarket/common/domain/email/controller/EmailController.java
@@ -21,7 +21,6 @@ public class EmailController {
 
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-
   @PostMapping("/mail")
   @ResponseBody
   public String emailConfirm(@RequestParam String email) throws Exception {
