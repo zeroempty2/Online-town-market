d4d269ba89c6daed9ad45f9a3a8d3d3dfe4a23fb
0empty <2zeroempty@gmail.com>
2023-03-07 12:46:50 +0900

    #241 <fix>: dto 생성자 변경으로 오류가 생기는 쿼리 수정

--- src/main/java/com/example/townmarket/common/domain/product/repository/ProductRepositoryQueryImpl.java
+++ src/main/java/com/example/townmarket/common/domain/product/repository/ProductRepositoryQueryImpl.java
@@ -46,6 +46,7 @@ public class ProductRepositoryQueryImpl implements ProductRepositoryQuery {
   public Page<PagingProductResponse> searchByKeyword(String keyword, Pageable pageable) {
     List<PagingProductResponse> pagingProductResponse = jpaQueryFactory
         .select(Projections.constructor(PagingProductResponse.class,
+            product.productImg,
             product.productName,
             product.productPrice,
             product.id))
